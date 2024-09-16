2067dd5ae9f67e3c112a41d53d17f1e1ce40e873
Marko Janosevic <m.janosevic@fitpassgroup.com>
2024-09-16 11:32:23 +0200

    Create models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -1,1 +1,9 @@
 <?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $fillable = ['venue_id', 'name', 'available_tickets', 'ticket_sales_end_date'];
+}
--- app/Models/TicketPurchase.php
+++ app/Models/TicketPurchase.php
@@ -1,1 +1,10 @@
 <?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TicketPurchase extends Model
+{
+    protected $fillable = ['event_id', 'email', 'transaction_id'];
+}
+
